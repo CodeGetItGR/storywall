951e2675defe8243bcd26130fccfe401304e813d
GiorgosTs19 <geontsou52@gmail.com>
2026-09-16 11:37:27 +0300

    Fixes.

--- components/feed/Banner.tsx
+++ components/feed/Banner.tsx
@@ -29,10 +29,6 @@ export function Banner({
                     ) : (
                         <BannerFallback actionHref={fallbackActionHref} actionLabel={fallbackActionLabel} />
                     )}
-                    <div
-                        className="pointer-events-none absolute inset-y-0 right-0 z-0 w-1/2 bg-white/10 [clip-path:polygon(24%_0,100%_0,100%_100%,0_100%)]"
-                        aria-hidden="true"
-                    />
                 </div>
                 <BannerOverlay title={title} actions={actions} glowVisible={glowVisible} />
             </div>
